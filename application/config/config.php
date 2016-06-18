4018509eee3f43f9e01a93122a38f9795d307a1f
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-06-18 20:22:55 +0700

    update base url

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   | a PHP script and you can easily do that on your own.
   |
  */
-$config['base_url'] = 'http://127.0.0.1/Metode-SAW/';
+$config['base_url'] = 'http://localhost/SPK-SAW/';
 
 /*
   |--------------------------------------------------------------------------

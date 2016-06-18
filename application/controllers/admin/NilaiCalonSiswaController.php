cfc6f8cbc0e5324a3fc07749cf8671b5c5dc2552
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-06-18 19:22:53 +0700

    update nilai calon siswa

--- application/controllers/admin/NilaiCalonSiswaController.php
+++ application/controllers/admin/NilaiCalonSiswaController.php
@@ -8,17 +8,16 @@
  * Project Metode-SAW
  * Package Expression package is undefined on line 14, column 14 in Templates/Scripting/PHPClass.php.
  */
-class NilaiCalonSiswaController extends CI_Controller
-{
-    public function __construct()
-    {
+class NilaiCalonSiswaController extends CI_Controller {
+
+    public function __construct() {
         parent::__construct();
         $this->load->model('NilaiCalonSiswa');
         $this->load->model('CalonSiswa');
+        $this->load->model('Himpunan');
     }
 
-    public function index()
-    {
+    public function index() {
         $session = $this->session->userdata('isLogin');
 
         if ($session == false) {
@@ -29,8 +28,7 @@ class NilaiCalonSiswaController extends CI_Controller
         }
     }
 
-    public function tambahNilaiCalonSiswa()
-    {
+    public function tambahNilaiCalonSiswa() {
         $nim = $this->input->post('nim');
         $c1 = $this->input->post('c1');
         $c2 = $this->input->post('c2');
@@ -38,8 +36,35 @@ class NilaiCalonSiswaController extends CI_Controller
         $c4 = $this->input->post('c4');
         $c5 = $this->input->post('c5');
 
+        foreach ($this->Himpunan->ambilHimpunan() as $h) {
+            if ($c1 >= $h->batas_atas and $c1 <= $h->batas_bawah) {
+                $c1 = $h->nilai;
+            }
+
+            if ($c2 >= $h->batas_atas and $c2 <= $h->batas_bawah) {
+                $c2 = $h->nilai;
+            }
+
+            if ($c3 >= $h->batas_atas and $c3 <= $h->batas_bawah) {
+                $c3 = $h->nilai;
+            }
+
+            if ($c4 >= $h->batas_atas and $c4 <= $h->batas_bawah) {
+                $c4 = $h->nilai;
+            }
+
+            if ($c5 >= $h->batas_atas and $c5 <= $h->batas_bawah) {
+                $c5 = $h->nilai;
+            }
+        }
+
         $val = array(
             'id_nilai' => $this->uuid->v4(),
+            'nilai_asli_c1' => $this->input->post('c1'),
+            'nilai_asli_c2' => $this->input->post('c2'),
+            'nilai_asli_c3' => $this->input->post('c3'),
+            'nilai_asli_c4' => $this->input->post('c4'),
+            'nilai_asli_c5' => $this->input->post('c5'),
             'c1' => $c1,
             'c2' => $c2,
             'c3' => $c3,
@@ -57,4 +82,5 @@ class NilaiCalonSiswaController extends CI_Controller
 
         redirect('admin/NilaiCalonSiswaController');
     }
+
 }

c608113f3dc60e3e5d436d883fb4204184064a43
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-06-19 10:03:24 +0700

    update header

--- application/views/admin/layout/HeaderLayout.php
+++ application/views/admin/layout/HeaderLayout.php
@@ -14,7 +14,7 @@
 
         <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
             <div class="menu_section">
-                <h3>General</h3>
+                <h3>Administrator</h3>
                 <ul class="nav side-menu">
                     <li>
                         <a href="<?php echo base_url(); ?>index.php/admin">

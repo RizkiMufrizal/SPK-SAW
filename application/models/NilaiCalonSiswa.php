ac6ead8e764034061c4258c06ef809d8ec010a80
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-06-19 09:54:04 +0700

    update model nilai calon siswa

--- application/models/NilaiCalonSiswa.php
+++ application/models/NilaiCalonSiswa.php
@@ -8,39 +8,32 @@
  * Project Metode-SAW
  * Package Expression package is undefined on line 14, column 14 in Templates/Scripting/PHPClass.php.
  */
-class NilaiCalonSiswa extends CI_Model
-{
-    public function ambilCalonSiswaDanNilai()
-    {
+class NilaiCalonSiswa extends CI_Model {
+
+    public function ambilCalonSiswaDanNilai() {
         return $this->db->get('tb_calon_siswa_nilai')->result();
     }
 
-    public function ambilNilaiCalonSiswaArray()
-    {
+    public function ambilNilaiCalonSiswaArray() {
         return $this->db->get('tb_calon_siswa_nilai')->result_array();
     }
 
-    public function tambahNilaiCalonSiswa($nilaiCalonSiswa)
-    {
+    public function tambahNilaiCalonSiswa($nilaiCalonSiswa) {
         $this->db->insert('tb_nilai_calon_siswa', $nilaiCalonSiswa);
     }
 
-    public function ambilNilaiCalonSiswaBerdasakanNim($nim)
-    {
-        $this->db->where('nim', $nim);
-
+    public function ambilNilaiCalonSiswaBerdasakanNisn($nisn) {
+        $this->db->where('nisn', $nisn);
         return $this->db->count_all_results('tb_calon_siswa_nilai');
     }
 
-    public function ambilJumlahNilaiCalonSiswa()
-    {
+    public function ambilJumlahNilaiCalonSiswa() {
         return $this->db->count_all_results('tb_nilai_calon_siswa');
     }
 
-    public function ambilNilaiMaxBerdasarkanKriteria($kriteria)
-    {
+    public function ambilNilaiMaxBerdasarkanKriteria($kriteria) {
         $this->db->select_max($kriteria);
-
         return $this->db->get('tb_nilai_calon_siswa')->result_array();
     }
+
 }

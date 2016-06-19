70edfcfe8b46063258b31f867cc5f1c66462b4f9
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-06-19 08:41:30 +0700

    update model himpunan

--- application/models/Himpunan.php
+++ application/models/Himpunan.php
@@ -12,8 +12,31 @@
  */
 class Himpunan extends CI_Model {
 
+    public function tambahHimpunan($himpunan) {
+        $this->db->insert('tb_himpunan', $himpunan);
+    }
+
     public function ambilHimpunan() {
+        $this->db->order_by('batas_atas', 'asc');
         return $this->db->get('tb_himpunan')->result();
+    }
+
+    public function ambilHimpunanBerdasarkanId($idHimpunan) {
+        $this->db->where('id_himpunan', $idHimpunan);
+
+        return $this->db->get('tb_himpunan')->result();
+    }
+
+    public function ubahHimpunan($himpunan, $idHimpunan) {
+        $this->db->where('id_himpunan', $idHimpunan);
+        $this->db->update('tb_himpunan', $himpunan);
+    }
+
+    public function hapusHimpunan($idHimpunan) {
+        $himpunan = array(
+            'id_himpunan' => $idHimpunan,
+        );
+        $this->db->delete('tb_himpunan', $himpunan);
     }
 
 }

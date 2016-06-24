9f40d23cd2c659a725b7a1a3eabd639cdbbf9474
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-06-24 22:18:10 +0700

    hapus bcrypt

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -59,7 +59,7 @@ $autoload['packages'] = array();
   |
   |	$autoload['libraries'] = array('user_agent' => 'ua');
  */
-$autoload['libraries'] = array('database', 'session', 'Uuid', 'bcrypt');
+$autoload['libraries'] = array('database', 'session', 'Uuid');
 
 /*
   | -------------------------------------------------------------------
